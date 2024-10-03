a515db55fa8fb9ae5999fc094870b51bffd4ce4c
jonathanlofgren1 <13JL0714@amal.se>
2024-10-03 09:40:41 +0200

    Added betting on colors.

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -5,73 +5,108 @@ Random rand = new Random();
 int pNumber = 0;
 string input = "";
 bool isPlaying = true;
+bool hasWon = false;
 
 
 while (isPlaying)
 {
-
+    hasWon = false;
     //slumpa
     int number = rand.Next(0, 36);
- 
+
 
 
 
 
 
     //get number and check if ok
-    Console.WriteLine("Choose number");
+    Console.WriteLine("Choose number or color.");
     bool validInput = false;
 
     while (!validInput)
     {
         input = Console.ReadLine();
 
-
-        pNumber = Convert.ToInt32(input);
-
-
-
-        if (pNumber <= 36)
+        if (input == "Red")
         {
-            Console.WriteLine("Your number is " + pNumber);
             validInput = true;
+            if (number % 2 == 0)
+            {
+                hasWon = true;
+            }
         }
-
-
-
-        if (pNumber > 36)
+        else if (input == "Black")
+        {
+            validInput = true;
+            if (number % 2 == 1)
+            {
+                hasWon = true;
+            }
+        }
+        else if (input == "Green")
+        {
+            validInput = true;
+            if (number == 0)
+            {
+                hasWon = true;
+            }
+        }
+        else
         {
             pNumber = Convert.ToInt32(input);
-            validInput = false;
-            Console.WriteLine("Your number is too big");
+            if (pNumber <= 36)
+            {
+                Console.WriteLine("Your number is " + pNumber);
+                validInput = true;
+            }
 
+
+
+            if (pNumber > 36)
+            {
+
+
+                validInput = false;
+                Console.WriteLine("Your number is too big");
+            }
+
+            Console.WriteLine("The number is " + number);
+
+
+        }
+    }
+
+    if (hasWon == true)
+    {
+        Console.WriteLine("You win!");
         
-        }
+    }
+    else 
+    {
+        Console.WriteLine("You lose!");
+
     }
 
-    //Win or lose anunsment
-    Console.WriteLine("The number is " + number);
 
+    //play again?
 
-        if (pNumber == number)
-        {
-            Console.WriteLine("You win!");
-        }
-        else if (number < pNumber || pNumber < number)
-        {
-            Console.WriteLine("You lose!");
-        }
-
-        //play again?
-
-        Console.WriteLine("Would you like to play again?");
-        input = Console.ReadLine();
-        if (input == "No")
-        {
-            isPlaying = false;
-        }
-
+    Console.WriteLine("Would you like to play again?");
+    input = Console.ReadLine();
+    if (input == "No")
+    {
+        isPlaying = false;
     }
+
+
+}
+
+
+//Win or lose anunsment
+
+
+
+
+
 
 
 
